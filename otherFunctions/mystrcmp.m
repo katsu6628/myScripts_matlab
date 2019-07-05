b1eeb0ad07283c9b9da263c201a4442f4e2361d8
katsu6628 <katsu6628@gmail.com>
2019-07-05 21:49:40 +0900

    a

--- otherFunctions/mystrcmp.m
+++ otherFunctions/mystrcmp.m
@@ -1,9 +1,9 @@
 function cmpLogic = strcmp_mult(baseString_cell, targetString_cell)
-cmpLogic = false(length(baseString_cell), 1);
+cmpLogic = zeros(size(baseString_cell));
 
 for idx = 1:length(targetString_cell)
    targetString = targetString_cell{idx};
-   
+   cmpLogic(strcmp(baseString_cell, targetString)) = idx;
 end
 
 end
