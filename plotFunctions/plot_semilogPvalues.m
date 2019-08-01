20cb98c730891d4298747b5a19fcf58c604aecf8
katsu6628 <katsu6628@gmail.com>
2019-08-01 16:35:16 +0900

    added semilog plot

--- plotFunctions/plot_semilogPvalues.m
+++ plotFunctions/plot_semilogPvalues.m
@@ -4,7 +4,7 @@ hold on
 set(gca,'YDir', 'reverse')
 % ylim([0 1])
 yticks([0.0001, 0.001, 0.01, 0.1, 1])
-xlabel('Time (s)');
+% xlabel('Time (s)');
 ylabel('p-value (log)');
 xlim([time_v(1) time_v(end)])
 plot(xlim, [0.05 0.05], 'k--');
